d757e9605f1f4303ef6e7860341a26d81b488c9e
Mau Vo <mau.vo.ym@bp.renesas.com>
2026-05-05 14:14:35 +0700

    fastboot: add checksum for eMMC bootloader update
    Compute CRC32 from read-back data after writing BL2/FIP to eMMC
    boot partition and expose it via fastboot getvar (crc32-bl2,
    crc32-fip).
    
    Signed-off-by: Mau Vo <mau.vo.ym@bp.renesas.com>

--- include/fb_mmc.h
+++ include/fb_mmc.h
@@ -68,7 +68,7 @@ void fastboot_mmc_erase(const char *cmd, char *response);
  *
  * @blk_start: Start block
  */
-int write_to_eMMC_bootpart(size_t blk_start);
+int write_to_eMMC_bootpart(size_t blk_start, uint32_t *out_checksum);
 
 /**
  * update_bootloader_to_eMMC() - Update bootloader for eMMC from the WIC image
